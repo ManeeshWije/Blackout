7f3ad4ea680045bde540c5fbe6a89f5e55e53f2b
Maneesh Wijewardhana <m.mwije1@gmail.com>
2021-03-15 01:41:11 -0400

    first commit

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,8 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Blackout")
-            .padding()
+        ZStack{
+            Image("background")
+            
+            VStack{
+                
+            }
+        }
     }
 }
 
@@ -19,3 +24,5 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
