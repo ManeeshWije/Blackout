a980b76cb51147aa3d5370869149aa43f3a358b5
Maneesh Wije <m.mwije1@gmail.com>
2021-03-14 23:55:10 -0400

    Initial Commit

---
+++ Shared/BlackoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlackoutApp.swift
+//  Shared
+//
+//  Created by Maneesh Wijewardhana on 2021-03-14.
+//
+
+import SwiftUI
+
+@main
+struct BlackoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
